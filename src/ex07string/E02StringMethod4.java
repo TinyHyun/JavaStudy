c46eca02dc2cdba00105ee4479a539352c67ae86
eodzhadl0128 <eodzhadl0128@naver.com>
2023-07-05 18:35:50 +0900

    연습문제 풀이

--- src/ex07string/E02StringMethod4.java
+++ src/ex07string/E02StringMethod4.java
@@ -10,8 +10,20 @@ public class E02StringMethod4 {
 		190419-3000000 => 남자
 		190419-4000000 => 여자
 		*/
-		String num1 = "190419-3000000";
-		String num2 = "190419-4000000";
+		String num = "190419-3000000";
+		
+		if(num.charAt(7)=='1' || num.charAt(7)=='3') {
+			System.out.println("남자입니다.");
+		}
+		else if(num.charAt(7)=='2' || num.charAt(7)=='4') {
+			System.out.println("여자입니다.");
+		}
+		else if(num.charAt(7)=='5' || num.charAt(7)=='6') {
+			System.out.println("외국인입니다.");
+		}
+		else {
+			System.out.println("주민번호를 잘못입력하셨습니다.");
+		}
 		
 		
 		
@@ -21,7 +33,18 @@ public class E02StringMethod4 {
 		시나리오2]주민등록번호로 성별을 구분하는 프로그램을 indexOf()를
 		이용해서 작성하시오.
 		*/
+		String jumin = "190419-200000";
+		int index = jumin.indexOf("-")+1; // 주민번호에 -다음으로 오는 인덱스의 숫자를 알기위해서 1을 더한다.
 		
+		if(jumin.charAt(index)=='1' || jumin.charAt(index)=='3') {
+			System.out.println("남자입니다.");
+		}
+		else if(jumin.charAt(index)=='2' || jumin.charAt(index)=='4') {
+			System.out.println("여자입니다.");
+		}
+		else if(jumin.charAt(index)=='5' || jumin.charAt(index)=='6') {
+			System.out.println("외국인입니다.");
+		}
 		
 		System.out.println("\n========================================\n");
 		
@@ -58,7 +81,9 @@ public class E02StringMethod4 {
 		파일명 : my.file.images.jpg
 		*/
 		String file = "my.file.images.jpg";
-		
+		int filename = file.lastIndexOf(".")+1;
+		String fileExt = file.substring(filename);
+		System.out.println("파일확장자: "+ fileExt);
 		
 	}
 
